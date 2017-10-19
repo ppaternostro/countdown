08dc1e8ebbf9b9b3b9a81a7cc938838246304c7e
ppaternostro <32653184+ppaternostro@users.noreply.github.com>
2017-10-18 20:49:09 -0400

    Add files via upload

---
+++ Util.java
@@ -0,0 +1,67 @@
+import java.io.*;
+
+import java.util.*;
+
+import javax.swing.*;
+
+public class Util
+{
+  private static String PROP_FILE = "countdown.properties";
+
+  public static void saveProperties(Properties prop) throws IOException
+  {
+    FileOutputStream fos = null;
+
+    try
+    {
+      fos = new FileOutputStream(PROP_FILE);
+      prop.store(fos,null);
+    }
+    finally
+    {
+      try
+      {
+        if (fos != null)
+        {
+          fos.close();
+        }
+      }
+      catch(IOException ioe)
+      {
+        //No blood, no foul!
+      }
+    }
+  }
+
+  public static Properties readProperties()
+  {
+    Properties prop = new Properties();
+    FileInputStream fis = null;
+
+    try
+    {
+      fis = new FileInputStream(PROP_FILE);
+      prop.load(fis);
+    }
+    catch(IOException ioe)
+    {
+      //Ignore as properties file may not yet exist.
+    }
+    finally
+    {
+      try
+      {
+        if (fis != null)
+        {
+          fis.close();
+        }
+      }
+      catch(IOException ioe)
+      {
+        //No blood, no foul!
+      }
+    }
+
+    return prop;
+  }
+}
