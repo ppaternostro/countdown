14b6d6b88982cc2f44a39ff6fd690de3d4150091
Pat Paternostro <ppaternostro@yahoo.com>
2026-05-06 21:49:33 -0400

    Strict resolver ensures date exists exactly as provided on the calendar

--- src/com/pasquasoft/tools/countdown/Util.java
+++ src/com/pasquasoft/tools/countdown/Util.java
@@ -1,19 +1,22 @@
 package com.pasquasoft.tools.countdown;
 
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
+import java.time.format.ResolverStyle;
 import java.util.Objects;
 
 public class Util
 {
   private static final String REGEX_TIME_MASK = "^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$";
-  private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+  private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("MM/dd/yyyy")
+      .withResolverStyle(ResolverStyle.STRICT);
 
   public static boolean isValidDate(String dateStr)
   {
     try
     {
-      FORMATTER.parse(dateStr);
+      LocalDate.parse(dateStr, FORMATTER);
     }
     catch (DateTimeParseException | NullPointerException e)
     {
